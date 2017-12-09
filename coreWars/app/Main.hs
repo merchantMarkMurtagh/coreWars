6a447788f4983102fdbe23c64d7f385fb52b3679
Mark Murtagh <mamurtag@tcd.ie>
2017-12-09 22:38:25 +0000

    input gains

--- coreWars/app/Main.hs
+++ coreWars/app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Lib
+import RedCode
+import System.IO
 
-main :: IO ()
-main = someFunc
+main = do readProgramFile
--- coreWars/src/RedCode.hs
+++ coreWars/src/RedCode.hs
@@ -1,28 +1,39 @@
 module RedCode where 
+import System.IO 
 
-data Instruction = Identity
-                 | Opcode Mode, Mode
-                 | Opcode Nothing Mode
-                 | Opcode Mode Nothing
-                   deriving(Show, Read)
- 
-
-type Mode = "#" ++ Integer
-           |"@" ++ Integer
-           | ">" ++ Integer
-           | Integer
-           | Nothing
-             deriving(Show, Read)
+data AddrMode =  Direct
+               | Indirect
+               | Immediate
+               | AutoDec
+                 deriving(Show, Read)
 
 data Opcode = Identity
-            | MOV 
+            | MOV
             | DAT
-            | ADD 
-            | SUB 
+            | ADD
+            | SUB
             | JMP
-            | JMZ 
-            | JMN 
-            | DJN 
-            | CMP 
+            | JMZ
+            | JMN
+            | DJN
+            | CMP
             | SPL
               deriving(Show, Read)
+
+data Instruction = Opcode (Maybe AddrInt) (Maybe AddrInt) 
+
+type AddrInt = (AddrMode, Integer)
+
+type Program = [Instruction]
+
+addInstruction :: Instruction ->  [Instruction] -> [Instruction]
+addInstruction instr x = instr:x 
+
+readProgramFile = do
+                handle <- openFile "src/program.txt" ReadMode  
+                contents <- hGetContents handle  
+                putStr contents  
+                hClose handle  
+
+
+makeProgram

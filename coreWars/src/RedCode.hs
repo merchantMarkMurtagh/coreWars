2c50b066d1591fba0534d950fd26aef5a1f9ea09
Mark Murtagh <mamurtag@tcd.ie>
2017-12-11 12:12:26 +0000

    yo

--- coreWars/src/RedCode.hs
+++ coreWars/src/RedCode.hs
@@ -20,7 +20,7 @@ data Opcode = Identity
             | SPL
               deriving(Show, Read)
 
-data Instruction = Opcode (Maybe AddrInt) (Maybe AddrInt) 
+type Instruction = (Opcode, AddrInt, AddrInt) 
 
 type AddrInt = (AddrMode, Integer)
 
@@ -30,10 +30,64 @@ addInstruction :: Instruction ->  [Instruction] -> [Instruction]
 addInstruction instr x = instr:x 
 
 readProgramFile = do
-                handle <- openFile "src/program.txt" ReadMode  
-                contents <- hGetContents handle  
-                putStr contents  
-                hClose handle  
+                withFile "src/program.txt" ReadMode (\handle -> do  
+                    contents <- hGetContents handle 
+                    let x = lines contents
+
+                    putStr contents) 
 
 
-makeProgram
+makeProgram :: [String] -> Program
+makeProgram x = [makeInstruction x]
+makeProgram x:xs = makeInstruction x:(makeProgram xs)
+
+
+makeInstruction :: [String] -> Instruction
+makeInstruction [a, b, c] = (op, (aField), (bField))
+                                where op = getOp a
+                                      aField = makeAddrInt (getAddrMode $ splitAddr b) (getL $ splitAddr b)
+                                      bField = makeAddrInt (getAddrMode $ splitAddr c) (getL $ splitAddr c)
+
+
+getOp :: String -> Opcode
+getOp "MOV" = MOV
+getOp "DAT" = DAT
+getOp "ADD" = ADD
+getOp "SUB" = SUB
+getOp "JMP" = JMP
+getOp "JMZ" = JMZ
+getOp "JMN" = JMN
+getOp "DJN" = DJN
+getOp "CMP" = CMP
+getOp "SPL" = SPL
+
+getAddrMode :: [String] -> AddrMode
+getAddrMode ["#", _ ] = Immediate
+getAddrMode ["@", _ ] = Indirect
+getAddrMode ["<", _ ] = AutoDec
+getAddrMode ["", _ ] = Direct
+
+makeAddrInt :: AddrMode -> Integer -> AddrInt
+makeAddrInt a b =  (a, b)
+
+getL :: [String] -> Integer
+getL [_, b, _] = (read b :: Integer)
+getL [_, b] = (read b :: Integer)
+
+getMode :: [String] -> String
+getMode [a, _, _] = a
+getMode [a, _] = a
+
+splitAddr :: String -> [String]
+splitAddr s = words s
+
+
+
+
+
+
+
+
+
+
+
